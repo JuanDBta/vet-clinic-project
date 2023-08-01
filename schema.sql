9d3669104f803abeb219d4dceb4c467221064a0c
JuanDBta <juandavid_diaz@hotmail.com>
2023-08-01 18:08:58 -0500

    Add owners data and species data to data.sql

--- schema.sql
+++ schema.sql
@@ -1,18 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-   
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL,
-  species_id TEXT,
-  owner_id TEXT
-  
-); 
-
 CREATE TABLE owners (
    
   id SERIAL PRIMARY KEY,
@@ -25,5 +12,18 @@ CREATE TABLE species (
    
   id SERIAL PRIMARY KEY,
   name TEXT
+  
+);
+
+CREATE TABLE animals (
+   
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  species_id INTEGER REFERENCES species(id),
+  owner_id INTEGER REFERENCES owners(id)
   
 ); 
