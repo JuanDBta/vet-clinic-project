60dda76749be600b6d772c3e551bb7267119f5f5
JuanDBta <juandavid_diaz@hotmail.com>
2023-08-02 09:34:01 -0500

    Fix first task in schema.sql

--- schema.sql
+++ schema.sql
@@ -23,7 +23,23 @@ CREATE TABLE animals (
   escape_attempts INTEGER,
   neutered BOOLEAN,
   weight_kg DECIMAL,
-  species_id INTEGER REFERENCES species(id),
-  owner_id INTEGER REFERENCES owners(id)
+  species TEXT
   
 ); 
+/* Modify animals table:
+Make sure that id is set as autoincremented PRIMARY KEY */
+ALTER TABLE animals DROP id;
+ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+
+/*Remove column species*/
+ALTER TABLE animals DROP COLUMN species;
+
+/*Add column species_id which is a foreign key referencing species table*/
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
+
+/*Add column owner_id which is a foreign key referencing the owners table*/
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
